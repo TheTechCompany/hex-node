4494623e473fb5c458033c43b98078b3f1be8bea
Ross Leitch <professional.balbatross@gmail.com>
2020-12-02 15:01:27 +1300

    initial p2p

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const PeerId = require('peer-id')
-const MDNS = require('libp2p-mdns')
+const Libp2p = require('libp2p')
+const TCP = require('libp2p-tcp')
+const WS = require('libp2p-websockets')
 
-let peer = PeerId.create()
-const mdns = new MDNS({peerId: peer})
+const MPLEX = require('libp2p-mplex')
+const {NOISE} = require('libp2p-noise')
+const MulticastDNS = require('libp2p-mdns')
+const DHT = require('libp2p-kad-dht')
+const GossipSub = require('libp2p-gossipsub')
 
-mdns.on('peer', (peerData) => {
-    console.log('Found a peer in the local network', peerData.id.toB58String(), peerData.multiaddrs)
 
-})
+async function startNode(){
+  const transportKey = WS.prototype[Symbol.toStringTag]
+  const node = await Libp2p.create({
+    modules: {
+      transport: [
+        TCP,
+        WS
+      ],
+      streamMuxer: [MPLEX],
+      connEncryption: [NOISE],
+      peerDiscovery: [MulticastDNS],
+      dht: DHT,
+      pubsub: GossipSub
+    },
+    config: {
+      peerDiscovery: {
+        autoDial: true,
+        [MulticastDNS.tag]: {
+          interval: 1000,
+          enabled: true
+        }
+      }
+    }
+  })
+  node.on('peer:discovery', (peer) => {
+    console.log('Discovered %s', peer.id.toB58String()) // Log discovered peer
+  })
 
-mdns.start()
 
+  await node.start()
+  console.log("Started node")
+}
+
+startNode()
